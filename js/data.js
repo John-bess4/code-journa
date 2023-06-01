7ceb9a0585dd5736f1b82967b00c44cd71aea936
John-bess4 <jbesss247@gmail.com>
2023-06-01 19:13:31 +0000

    create an entry

--- js/data.js
+++ js/data.js
@@ -6,3 +6,13 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+window.addEventListener('beforeunload', function () {
+  const jsonData = JSON.stringify(data);
+  localStorage.setItem('dataModel', jsonData);
+});
+
+if (localStorage.getItem('dataModel')) {
+  const storedData = JSON.parse(localStorage.getItem('dataModel'));
+  data = storedData;
+}
